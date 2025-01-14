18eb49d45a86599d5f5709907adcf31f38c8437c
Mounir Akhsassi <111659459+Mounir1999@users.noreply.github.com>
2025-01-14 15:05:14 +0100

    music Player directory

--- music Player/app/src/main/java/music/saidweb/playlist/offline/Ringtone/StoragePermission.java
+++
@@ -1,6 +0,0 @@
-package music.saidweb.playlist.offline.Ringtone;
-
-public interface StoragePermission {
-    void storageValidation(boolean isStorageGranted);
-    void setRingtone();
-}
--- music Player/app/src/main/java/music/saidweb/playlist/offline/Utility.java
+++
@@ -1,6 +0,0 @@
-package music.saidweb.playlist.offline;
-
-public class Utility {
-    public static int currentPosition;
-    public static String currentSongPath;
-}
